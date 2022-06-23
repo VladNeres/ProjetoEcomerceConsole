b4c5d6c6e20fd421ceee36851194ad26ebb618f0
Vladmir Neres <vneres@cleartech.dev>
2022-06-23 09:55:36 -0300

    cirando a branch main

---
+++ CadastrarCategorias1/Categorias.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace CadastrarCategorias
+{
+    public class Categorias
+    {
+
+        public string nome;
+        public string status;
+        public string data_Hora;
+
+     //creating constructor just allown create a new object if the person type the name of category
+        public Categorias(string nomeDaCategoria)
+        {
+            nome = nomeDaCategoria;
+            Verificar_Letras();
+        }
+
+        // verification about limite  and alphabet
+        public void Verificar_Letras()
+        {
+
+            while (!Regex.IsMatch(nome, @"^[ a-zA-Z á]*$") || nome.Length < 1 || nome.Length > 10)
+            {
+
+                if (!Regex.IsMatch(nome, @"^[ a-zA-Z á]*$"))
+                {
+                    Console.WriteLine("por favor digite apenas letras no nome da categoria");
+                    
+                }
+                else if (nome.Length < 1 || nome.Length > 5)
+                {
+                    Console.WriteLine("tamanho insuficiente ou maior do que o esperado\n digite de 1 ate 125 caraacteres");
+                    nome = Console.ReadLine();
+                }                           
+            } 
+            Data_hora();
+        }
+        public void Data_hora()
+        {
+            data_Hora = DateTime.Now.ToString("dddd, dd MMMM yyyy HH:mm:ss");
+            Console.WriteLine(" A Categoria: " + nome + ",  foi criado :" + data_Hora);
+        }
+
+    }
+}
---
+++ CadastrarCategorias1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CadastrarCategorias
+{
+    class program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Ola Seja Bem vindao ao sisteme.\nDigide o nome da Categoria");
+
+            string nome = Console.ReadLine();
+            Categorias categoria = new Categorias(nome); 
+
+            
+        }
+    }
+}

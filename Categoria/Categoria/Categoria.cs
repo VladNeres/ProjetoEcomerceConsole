1f2df25dffe5df1b3fe181dd7b007e597c9c7e32
Vladmir Neres <vneres@cleartech.dev>
2022-07-18 09:21:49 -0300

    Refatorando o codigo

---
+++ Categoria/Categoria/Categoria.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Categorias
+{
+    public class Categoria
+    {
+        public string Nome { get; protected set; }
+        public string Status { get; protected set; }  
+        public DateTime Data_hora { get; protected set; } 
+
+
+        public Categoria()
+        {
+
+        }
+
+
+        public virtual string CadastrarCategoria()
+        {
+            bool loop = true;
+            while (loop)
+            {
+                Console.WriteLine("Digite o nome da Categoria: ");
+                string nomeCategoria = Console.ReadLine();
+                if (VerificarLetras(nomeCategoria))
+                {
+                    Nome = nomeCategoria;
+
+                    Console.WriteLine("O nome da categoria é : " + Nome);
+                    Console.WriteLine("Criada em :  " + (Data_hora = DateTime.Now));
+                    Console.WriteLine("Status : " + (Status = " Ativo"));
+                    loop = false;
+                }
+                else
+                {
+                    Console.WriteLine("A categoria deve conter de 1 a 128 carcteres (apenas letras)");
+                }
+               
+            }
+            return "Categoria criada com sucesso\n";
+        }
+
+        public bool VerificarLetras(string nome)
+        {
+
+            int regex = Regex.Matches(nome, @"[a-zA-Zá-úÁ-Ú' ']").Count;
+                if (!String.IsNullOrWhiteSpace(nome) && nome.Length >0 && nome.Length <= 18 && regex == nome.Length ) 
+                {
+                    return true;
+                }   
+                return false;
+        }
+         public virtual string EditarCategoria()
+         {
+            Console.WriteLine("Escreva o novo nome da categoria");
+            string novonome = Console.ReadLine();
+            bool loopEditar = true;
+            while (loopEditar)
+            {
+                if (VerificarLetras(novonome))
+                {
+                    Console.WriteLine("A categoria : " + Nome + " foi alterada na data: " + Data_hora + " para : " + (Nome = novonome));
+                    Console.WriteLine("Data de alteração : " + (Data_hora = DateTime.Now));
+                    Console.WriteLine("O Status da Categoria está : " + Status);
+                    loopEditar = false;
+                }
+            }            
+            return "Categoria atualizada com sucesso \n";
+
+
+         }
+    }
+
+}
+
---
+++ Categoria/Categoria/Menu.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Categorias
+{
+    internal class Menu : Categoria
+    {
+
+        public static void MenuNavegar()
+        {
+            Categoria categoria = new Categoria();
+            SubCategoria subCategoria = new SubCategoria();
+
+            bool loopMenu = true;
+            while (loopMenu)
+            {
+                Console.WriteLine("Digite a opção desejada: \n" +
+                                                            "1- Cadastrar categoria \n" +
+                                                            "2- Editar categoria \n" +
+                                                            "3- Cadastrar subcategoria \n" +
+                                                            "4- Editar subCategoria \n" +
+                                                            "0- Para sair");
+
+
+                
+                    
+
+                    string opcaoMenu = Console.ReadLine();
+                    switch (opcaoMenu)
+                    {
+                        case "1":
+                        
+                            Console.WriteLine(categoria.CadastrarCategoria());
+                            break;
+
+      
+                        case "2":
+
+                        try
+                        {
+                            Console.WriteLine(categoria.EditarCategoria());
+
+                        }
+                        catch(System.NullReferenceException)
+                        {
+                            Console.WriteLine("Não pode criar essa joça");
+                            break;
+                        }
+                        break;
+
+                    case "3":
+                        if (string.IsNullOrEmpty(categoria.Nome))
+                        {
+                            Console.WriteLine("Para cadastrar uma subcategoria é necessario ter uma categoria cadastrada.\n" +
+                                              "por favor primeiro cadastre uma categoria!\n");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine(subCategoria.CadastrarCategoria());
+                            break;
+                        }
+
+                    case "4":
+
+                        if (string.IsNullOrEmpty(subCategoria.Nome))
+                        {
+                            Console.WriteLine("Não há subcategoria cadastrada, por favor primeiro cadastre uma categoria!\n ");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine(subCategoria.EditarCategoria());
+                            break;
+
+                        }
+
+                    case "0":
+                            Console.WriteLine("Obrigado por utilizar nosso Sistema");
+                            loopMenu = false;
+                        break;
+
+                    default:
+                            Console.WriteLine("Opção invalida, digite uma das opções disponíveis");
+                            break;
+                    }
+
+                
+
+            }
+        }
+
+    }
+}

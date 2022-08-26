2356515889e316d3d8ea0cd13231a1a872005f58
Vladmir Neres <vneres@cleartech.dev>
2022-08-26 20:37:41 -0300

    Criando projeto em Api, Criando a classe de Categoria, metodo Post, metodo getLista, GetListaporID e conexão com banco de dados

---
+++ CategoriaApi/CategoriaApi/Controllers/CategoriaController.cs
@@ -0,0 +1,50 @@
+﻿
+
+using CategoriaApi.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CategoriaApi.Controllers
+{
+        [ApiController]
+        [Route("[controller]")]
+    public class CategoriaController : ControllerBase
+    {
+        private static int Id = 1;
+        private static List<Categoria> categorias = new List<Categoria>();
+
+        [HttpPost]
+        public IActionResult AdicionarCategoria([FromBody] Categoria categoria)
+        {
+            if (categoria.Status== true)
+            {
+                categoria.Id = Id++;
+                categorias.Add(categoria);
+                Console.WriteLine(categoria.Nome);
+                return CreatedAtAction(nameof(GetCategoriaPorId), new { id = categoria.Id }, categoria);
+            }
+            else
+            return BadRequest("A categoria deve ser criada com o status (true)");
+        }
+
+        [HttpGet ]
+        public IActionResult GetCategoria()
+        {
+            return Ok(categorias);
+        }
+
+
+        [HttpGet("{Id}")]
+        public IActionResult GetCategoriaPorId(int id)
+        {
+                Categoria categoria= categorias.FirstOrDefault(categoria => categoria.Id == id);
+            if (categoria != null)
+            {
+                return Ok(categoria);
+            }
+            return NotFound("Não encontrado");
+        }
+    }
+}
---
+++ CategoriaApi/CategoriaApi/Data/CategoriaContext.cs
@@ -0,0 +1,15 @@
+﻿using CategoriaApi.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CategoriaApi.Data
+{
+    public class CategoriaContext: DbContext
+    {
+        public CategoriaContext(DbContextOptions<CategoriaContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
